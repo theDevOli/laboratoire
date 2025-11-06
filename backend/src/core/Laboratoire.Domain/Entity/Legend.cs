800a91dd78f672488c05e1d043f17d9c035443b4
Daniel Oliveira <ca.daniel@outlook.com>
2025-11-06 11:30:07 -0300

    🐛 fix(Legend): Override both Equals and GetHashCode

--- backend/src/core/Laboratoire.Domain/Entity/Legend.cs
+++ backend/src/core/Laboratoire.Domain/Entity/Legend.cs
@@ -12,4 +12,16 @@ public class Legend
     [JsonPropertyName("description")]
     public string? Description { get; set; }
 
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != this.GetType())
+            return false;
+        Legend? other = obj as Legend;
+
+        return other?.Unit == this.Unit && other?.Description == this.Description;
+    }
+
+    public override int GetHashCode()
+    => HashCode.Combine(this.Unit, this.Description);
+
 }
